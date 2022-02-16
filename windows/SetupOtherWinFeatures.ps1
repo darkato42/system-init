c20729134d757e919f06ca7329bbcab0461ebc74
Yulin Wu <darkato@icloud.com>
2022-02-16 21:40:53 +0000

    Create SetupOtherWinFeatures.ps1

---
+++ windows/SetupOtherWinFeatures.ps1
@@ -0,0 +1,27 @@
+# This script installs IIS and the features 
+#
+# * Make sure you run this script from a Powershel Admin Prompt!
+# * Make sure Powershell Execution Policy is bypassed to run these scripts:
+# * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
+# Set-ExecutionPolicy Bypass -Scope Process
+
+# To list all Windows Features: dism /online /Get-Features
+# Get-WindowsOptionalFeature -Online 
+# LIST All IIS FEATURES: 
+# Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
+
+# Containers
+Enable-WindowsOptionalFeature -Online -FeatureName Containers
+Enable-WindowsOptionalFeature -Online -FeatureName Containers-DisposableClientVM
+
+# Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Hypervisor
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Services
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Clients
+
+# Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux

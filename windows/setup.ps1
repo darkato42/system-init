a1830f0b05f642aa78711256e03cc36be2c1f534
Yulin Wu <darkato@icloud.com>
2022-02-16 21:15:59 +0000

    copied from aaronpowell/system-init

---
+++ windows/setup.ps1
@@ -0,0 +1,59 @@
+function Install-Chocolatey {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function Install-FromChocolatey {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $PackageName
+    )
+
+    choco install $PackageName --yes
+}
+
+function Install-PowerShellModule {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $ModuleName,
+
+        [ScriptBlock]
+        [Parameter(Mandatory = $true)]
+        $PostInstall = {}
+    )
+
+    if (!(Get-Command -Name $ModuleName -ErrorAction SilentlyContinue)) {
+        Write-Host "Installing $ModuleName"
+        Install-Module -Name $ModuleName -Scope CurrentUser -Confirm $true
+        Import-Module $ModuleName -Confirm
+
+        Invoke-Command -ScriptBlock $PostInstall
+    } else {
+        Write-Host "$ModuleName was already installed, skipping"
+    }
+}
+
+Install-Chocolatey
+
+Install-FromChocolatey 'git'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/darkato42/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
+
+Install-FromChocolatey 'vscode'
+Install-FromChocolatey 'dotnetcore-sdk'
+Install-FromChocolatey 'microsoft-windows-terminal'
+Install-FromChocolatey 'fiddler'
+Install-FromChocolatey 'postman'
+Install-FromChocolatey 'powershell-core'
+
+Install-PowerShellModule 'Posh-Git' { Add-PoshGitToProfile -AllHosts }
+Install-PowerShellModule 'oh-my-posh' { }
+Install-PowerShellModule 'PSReadLine' { }
+Install-PowerShellModule 'nvm' {
+    Install-NodeVersion latest
+    Set-NodeVersion -Persist User latest
+}
+
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/darkato42/system-init/master/windows/Microsoft.PowerShell_profile.ps1' -OutFile $PROFILE

16c4785c4604da7ff4f6a69ce0a59e2fa3bee5d0
Yulin Wu <darkato@icloud.com>
2022-02-16 21:31:28 +0000

    Update SetupIIS.ps1

--- windows/SetupIIS.ps1
+++ windows/SetupIIS.ps1
@@ -3,7 +3,7 @@
 # * Make sure you run this script from a Powershel Admin Prompt!
 # * Make sure Powershell Execution Policy is bypassed to run these scripts:
 # * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
-Set-ExecutionPolicy Bypass -Scope Process
+# Set-ExecutionPolicy Bypass -Scope Process
 
 # To list all Windows Features: dism /online /Get-Features
 # Get-WindowsOptionalFeature -Online 

fdb88dde367d32f1d81c1b19a485863b48570a1f
Yulin Wu <darkato@icloud.com>
2022-03-09 22:22:03 +0000

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-Import-Module posh-git -Scope CurrentUser -Force
-Import-Module oh-my-posh -Scope CurrentUser -Force
+Import-Module posh-git -Scope Local -Force
+Import-Module oh-my-posh -Scope Local -Force
 Set-PoshPrompt Paradox
 
-Import-Module nvm -Scope CurrentUser -Force
+Import-Module nvm -Scope Local -Force

52c4ffd33f8bd0fbf358fdad7ef836cb7cf79da9
Yulin Wu <darkato@icloud.com>
2022-03-09 23:09:15 +0000

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Import-Module posh-git -Scope Local -Force
 Import-Module nvm -Scope Local -Force
+Import-Module Terminal-Icons -Scope Local -Force
 oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/agnoster.omp.json | Invoke-Expression

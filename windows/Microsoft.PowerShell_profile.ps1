5e24318151657b0b5eea7cc697f931081f16a3ca
Yulin Wu <darkato@icloud.com>
2022-03-09 22:28:38 +0000

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
 Import-Module posh-git -Scope Local -Force
-Import-Module oh-my-posh -Scope Local -Force
-Set-PoshPrompt Paradox
-
 Import-Module nvm -Scope Local -Force
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression

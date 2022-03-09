e8e4002a73f8b65a180de0b51fc42e2118fb0412
Yulin Wu <darkato@icloud.com>
2022-03-09 22:59:18 +0000

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 Import-Module posh-git -Scope Local -Force
 Import-Module nvm -Scope Local -Force
-oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cloud-native-azure.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/agnoster.omp.json | Invoke-Expression
